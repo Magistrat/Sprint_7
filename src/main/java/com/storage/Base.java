cbb83de21fd19962f487012a05d49813586d881c
Magistrat <nisraton@gmail.com>
2023-09-10 21:22:59 +0300

    [feature] move from Base to Interfaces

--- src/main/java/com/storage/Base.java
+++ src/main/java/com/storage/Base.java
@@ -1,22 +1,6 @@
 package com.storage;
 
 public class Base {
-    private static final String BASE_URL = "http://qa-scooter.praktikum-services.ru/";
-    private static final String CONTENT_TYPE = "Content-Type";
-    private static final String APPLICATION_JSON = "application/json";
-
-    public static String getBaseUrl(){
-        return BASE_URL;
-    }
-
-    public static String getContentType(){
-        return CONTENT_TYPE;
-    }
-
-    public static String getApplicationJson(){
-        return APPLICATION_JSON;
-    }
-
     public static String generateTestData(String startData) {
         long unixTime = System.currentTimeMillis();
         return startData + "_" + unixTime;
