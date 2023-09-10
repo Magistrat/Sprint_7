5a114fe94f56251bbdfd73f7abd915ab8a2e7c00
Magistrat <nisraton@gmail.com>
2023-09-10 21:22:33 +0300

    [feature] add Interface with settings

---
+++ src/main/java/com/storage/SettingsInterface.java
@@ -0,0 +1,9 @@
+package com.storage;
+
+public interface SettingsInterface {
+    public static final String BASE_URL = "http://qa-scooter.praktikum-services.ru/";
+    public static final String CREATED_COURIER_URL = "/api/v1/courier";
+    public static final String CONTENT_TYPE = "Content-Type";
+    public static final String APPLICATION_JSON = "application/json";
+    public static final int CREATED_STATUS_CODE = 201;
+}
